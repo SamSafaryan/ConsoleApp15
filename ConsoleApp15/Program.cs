0ef2c8476616ad057b0d48ee7c66b0a631ab38c6
Samvel Safaryan <samvelsafaryan7@gmail.com>
2023-06-04 19:31:09 +0400

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int Count = 0;
+            int Sum = 0;
+            int number = Number;
+            int Mult = 1;
+            int Number_ = Number;
+            while(number > 0)
+            {
+                number /= 10;
+                Count++;
+            }
+            while (Number > 0)
+            {
+                number = Number %= 10;
+                for(int i = 0; i < Count; i++)
+                {
+                    Mult *= number;
+                    Sum += Mult;
+                }
+                number = Number /= 10;
+            }
+            Console.WriteLine(Sum);
+            if (Sum == Number)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False"); 
+            }
+        }
+    }
+}
